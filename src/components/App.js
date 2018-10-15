c21b41d758c3371a6badf6c6a8ce0906ef53d6d7
Ryan Webb <webbryan92@gmail.com>
2018-10-15 19:08:51 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+
+import Navigation from "./Navigation";
+import LandingPage from "./Landing";
+import SignUpPage from "./SignUp";
+import SignInPage from "./SignIn";
+import PasswordForgetPage from "./PasswordForget";
+import HomePage from "./Home";
+import AccountPage from "./Account";
+
+import * as routes from "../constants/routes";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Navigation />
+          <hr />
+
+          <Route exact path={routes.LANDING} component={LandingPage} />
+          <Route exact path={routes.SIGN_UP} component={SignUpPage} />
+          <Route exact path={routes.SIGN_IN} component={SignInPage} />
+          <Route
+            exact
+            path={routes.PASSWORD_FORGET}
+            component={PasswordForgetPage}
+          />
+          <Route exact path={routes.HOME} component={HomePage} />
+          <Route exact path={routes.ACCOUNT} component={AccountPage} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
